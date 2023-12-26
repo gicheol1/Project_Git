390b5bcde7bd834782ae8211f1eb7a24aac4b4c8
lswcw <lswcw@naver.com>
2023-12-26 20:54:31 +0900

    추가 적용

--- Front/project_Full/src/js/layout/header/Header.js
+++ Front/project_Full/src/js/layout/header/Header.js
@@ -1,7 +1,7 @@
 
 import { Link, useNavigate } from "react-router-dom";
 import { useLogOut } from "./useLogOut";
-
+import './Header.css';
 
 const Header = ({ isLogin, isAdmin, setIsLogin, isVisible }) => {
 
@@ -36,16 +36,16 @@ const Header = ({ isLogin, isAdmin, setIsLogin, isVisible }) => {
 
             {/* 메뉴 */}
             <ul className="nav col align-items-center justify-content-center col-md-auto mb-2 mb-md-0">
-                <li><Link to="/" className="nav-link px-2 link-dark">홈</Link></li>
-                <li><Link to="*" className="nav-link px-2 link-dark">게시판</Link></li>
-                <li><Link to="/travelKaKaoMap" className="nav-link px-2 link-dark">여행지도</Link></li>
-                <li><Link to="/calendar" className="nav-link px-2 link-dark">여행달력</Link></li>
-                <li><Link to="/packreservationList" className="nav-link px-2 link-dark">여행상품</Link></li>
-                <li><Link to="/connectionlog" className="nav-link px-2 link-dark">⏱ 최근 본 내역</Link></li>
+                <li><Link to="/" className="nav-link px-3 link-dark">홈</Link></li>
+                <li><Link to="*" className="nav-link px-3 link-dark">게시판</Link></li>
+                <li><Link to="/travelKaKaoMap" className="nav-link px-3 link-dark">여행지도</Link></li>
+                <li><Link to="/calendar" className="nav-link px-3 link-dark">여행달력</Link></li>
+                <li><Link to="/packreservationList" className="nav-link px-3 link-dark">여행상품</Link></li>
+                <li><Link to="/connectionlog" className="nav-link px-3 link-dark">⏱ 최근 본 내역</Link></li>
                 {isAdmin && (
                     <>
-                        <li><Link to="/festivalList" className="nav-link px-2 link-dark">축제관리</Link></li>
-                        <li><Link to="/travalpackadd" className="nav-link px-2 link-dark">패키지관리</Link></li>
+                        <li><Link to="/festivalList" className="nav-link px-3 link-dark">축제관리</Link></li>
+                        <li><Link to="/travalpackadd" className="nav-link px-3 link-dark">패키지관리</Link></li>
                         {/* <li><Link to="/Paymenthistory" className="nav-link px-2 link-dark">결제 내역</Link></li> */}
                     </>
                 )}
@@ -55,17 +55,17 @@ const Header = ({ isLogin, isAdmin, setIsLogin, isVisible }) => {
             {isLogin ?
                 <div className="col-md-5 text-end">
                     <ul className="nav col-md-auto justify-content-center mb-md-0">
-                        <li><button onClick={toLogout} class="btn btn-primary">로그아웃</button></li>
-                        <li><Link to='/myPage' className="btn btn-outline-primary me-2">👤 마이페이지</Link></li>
-                        <li><Link to='/mycart' className="btn btn-outline-primary me-2">🛒 장바구니</Link></li>
-                        <li><Link to='/packreservation/memberpackreservation' className="btn btn-outline-primary me-2">⏱ 예약 목록</Link></li>
-                        <li><Link to="/Paymenthistory" className="btn btn-outline-primary me-2">결제 내역</Link></li>
+                        <li><button onClick={toLogout} class="btn btn-primary me-3">로그아웃</button></li>
+                        <li><Link to='/myPage' className="btn btn-outline-primary me-3">👤 마이페이지</Link></li>
+                        <li><Link to='/mycart' className="btn btn-outline-primary me-3">🛒 장바구니</Link></li>
+                        <li><Link to='/packreservation/memberpackreservation' className="btn btn-outline-primary me-3">⏱ 예약 목록</Link></li>
+                        <li><Link to="/Paymenthistory" className="btn btn-outline-primary me-3">결제 내역</Link></li>
                     </ul>
                 </div>
                 :
                 <div className="col-md-2 text-end">
-                    <ul className="nav col-md-auto justify-content-center mb-md-0">
-                        <li><Link to='/login' className="btn btn-primary">로그인</Link></li>
+                    <ul className="nav col-md-auto justify-content-center mb-md-0 ">
+                        <li><Link to='/login' className="btn btn-primary">로 그 인</Link></li>
                     </ul>
                 </div>
             }
