b90f88cde83e078b504a63e67c51a94da3b467e2
Lewis_Sanli <davidsanli0510@gmail.com>
2023-12-26 18:32:47 +0900

    메인페이지 최종

--- Front/project_Full/src/js/pages/MainPage/MainPage.js
+++ Front/project_Full/src/js/pages/MainPage/MainPage.js
@@ -2,23 +2,50 @@
 import './MainPage.css';
 import { SERVER_URL } from 'js';
 import React,{ useEffect,useState, useCheckLogin } from "react";
+import { useBoardDetail } from "../board/boardDetail/useBoardDetail";
+import { useParams } from "react-router-dom";
+import axios from 'axios';
+
+const API_KEY = 'd520697c0f0c14123d8ef96d9ca14194';
+
 
 function MainPage() {
 
   const [packreservation, setPackreservation] = useState([]); // 패키지 여행 예약 목록
   const [userName, setUserName] = useState(''); // 회원 이름 
   const [test,setTest] = useState([]);
+  const [region,setRegion] = useState('');
+  const { isOwnerBoard } = useBoardDetail();
+	const [board, setBoard] = useState({});
+	const { target, boardNum } = useParams();
+	const [isOwner, setIsOwner] = useState(false);
+  const [weatherData, setWeatherData] = useState(null);
+  const [city, setCity] = useState('');
 
+  const API_KEY = 'd520697c0f0c14123d8ef96d9ca14194';
+
+
+
+  const getWeatherData = async () => {
+    try {
+      const response = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${'republic of korea'}&appid=${API_KEY}`
+      );
+      setWeatherData(response.data);
+    } catch (error) {
+      console.error('Error fetching weather data:', error);
+    }
+  };
 
   useEffect(() => { 
     
     MyBoard();
- 
+    getWeatherData();
+
   }, [])
 
   const MyBoard = () =>{
     const jwt = sessionStorage.getItem('jwt');
-
 
         fetch(SERVER_URL + `getUser?jwt=${jwt}`, { method: 'GET' })
             .then(response => response.json())
@@ -27,17 +54,23 @@ function MainPage() {
                 /* 회원이 예약한 패키지 여행 내역 데이터 가져오기 */
                 fetch(SERVER_URL + `packreservation/memberpackreservation/${data.memId}`, { method: 'POST' })
                     .then(response => response.json())
+                
                     .then(data => { setPackreservation(data); console.log(data); 
                     setTest(data);})
                     .catch(err => { console.error(err); });
-
+                    
                 /* >로그인한 회원의 이름을 출력하기위한 코드 */
                 // 회원의 이름을 가져온다고 가정하고, data에 이름이 담겨있다고 가정합니다.
                 const memberName = data.name; // data에서 이름 필드를 가져온다
                 setUserName(memberName);
- 
+                const regionName = data.addrRoad;
+                setRegion(regionName);
+                console.log(data);
             }).catch(err => console.error(err));
+
+            
   }
+
 
 
     return (
@@ -46,12 +79,12 @@ function MainPage() {
         <div class="mycol1">
         <div class="cardHeader">
             <h4>{userName}님의 예약정보</h4>
-            {console.log(userName)}
           </div>
           <div class="cardBody">
           {packreservation.map(data => 
           <div>
-            <p>{data.packName}</p>
+            <h5>{data.packName}</h5>
+            <p>예약날짜: {data.startDate}</p>
             </div>
           )}
           
@@ -61,59 +94,49 @@ function MainPage() {
           </div>
 
           <div class="mycol2">
-        <div class="cardHeader">
-            <h4 class="my-0 fw-normal">오늘 본 상품 보기</h4>
-            </div>
-
-          <div class="cardBody">
-            <ul class="list-unstyled mt-3 mb-4">
-              <li>10 users included</li>
-              <li>2 GB of storage</li>
-              <li>Email support</li>
-              <li>Help center access</li>
-            </ul>
+          <div class="cardHeader">
+            <h4>한국 날씨</h4>
           </div>
+      {weatherData && (
+        <div>
+          <br />
+              <br />
+              <br />
+          <h2>온도: {Math.floor(weatherData.main.temp-273.15)} 도</h2>
+        </div>
+      )}
+    </div>
           </div>
 
           <div class="mycol3">
         <div class="cardHeader">
-            <h4 class="my-0 fw-normal">지역</h4>
+            <h4>{userName}님의 지역</h4>
             </div>
 
           <div class="cardBody">
             <ul class="list-unstyled mt-3 mb-4">
-              <li>10 users included</li>
-              <li>2 GB of storage</li>
-              <li>Email support</li>
-              <li>Help center access</li>
+              <br />
+              <br />
+              <br />
+              <h2>{region}</h2>
             </ul>
           </div>
           </div>
-        </div>
+    <div class='MypageThird'>
+    <div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-body-tertiary">
+    <div class="col-md-6 p-lg-5 mx-auto my-5">
+      <h1 class="display-3 fw-bold">방문해주셔서 감사합니다.</h1>
+      <h3 class="fw-normal text-muted mb-3">Thank you for visiting</h3>
+      <div class="d-flex gap-3 justify-content-center lead fw-normal">
+       
+      </div>
+    </div>
+    <div class="product-device shadow-sm d-none d-md-block"></div>
+    <div class="product-device product-device-2 shadow-sm d-none d-md-block"></div>
+  </div>
 
-        <div class="MypageSecond">
-          
-          <div class="MyBoard">
-            <div class="MyBoardHeader">
-              <h4 align="center">작성한 게시글 보기</h4>
-            </div>
-            <div class="MyBoardBody"></div>
-              
-          </div>
+  </div>
 
-
-          <div class="MyHeart">
-
-
-            <div class="MyHeartHeader">
-              <h4 align="center">좋아요 글 보기</h4>
-            </div>
-            
-            <div class="MyHeartBody"></div>
-  
-            </div>
-
-          </div>
 
         </div>
 
