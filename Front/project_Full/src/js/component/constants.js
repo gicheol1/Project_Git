db42e325a9e92941599c395399e53d30b3fc942e
HERO216 <1761040@pcu.ac.kr>
2024-01-05 11:35:58 +0900

    [서버 배포] - 백엔드, 프론트 엔드 서버 배포 용 주소 추가

--- Front/project_Full/src/js/component/constants.js
+++ Front/project_Full/src/js/component/constants.js
@@ -1,3 +1,4 @@
 export const SERVER_URL = 'http://localhost:8090/'; // 테스트용 서버 포트
 // export const SERVER_URL = 'http://localhost:5000/'; // 배포시 적용할 서버 포트
-// export const SERVER_URL = 'http://viva-la-fiesta-env.ap-northeast-2.elasticbeanstalk.com/'; // 도메인 미적용 서버 배포
+// export const SERVER_URL = 'http://viva-la-fiesta-env.ap-northeast-2.elasticbeanstalk.com/'; // 도메인 미적용 서버 배포(ap-northeast-2 한국)
+// export const SERVER_URL = 'https://back.viva-la-fiestas.com/'; // 도메인 적용 서버 배포(us-east-1 외국)
